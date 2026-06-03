d381c37367e8def295166ad97dbd7cb54ca009ec
Satwik1Anand <satwik.anand@infogain.com>
2026-06-03 09:53:53 +0530

    [PROMPT-20260603042226] create a classic Snake Game in python using pygame with-key controls,food spawni

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,25 +2,6 @@ class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]  # A list to hold the board state
         self.current_player = 'X'
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]  # A list to hold the board state
-        self.current_player = 'X'}}
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]  # A list to hold the board state
-        self.current_player = 'X'}}]}  
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]  # A list to hold the board state
-        self.current_player = 'X'
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]  # A list to hold the board state
-        self.current_player = 'X'
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]  # A list to hold the board state
-        self.current_player = 'X' 
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]  # A list to hold the board state
-        self.current_player = 'X'
-
 def play(self):
         while True:
             self.print_board()
@@ -42,11 +23,9 @@ def play(self):
 if __name__ == '__main__':
     game = TicTacToe()
     game.play()
-
             def __init__(self):
         self.board = [' ' for _ in range(9)]  # A list to hold the board state
         self.current_player = 'X'
-
 def play(self):
         while True:
             self.print_board()
@@ -64,16 +43,13 @@ def play(self):
                 self.switch_player()
             else:
                 print('Invalid move. Try again.')
-
     def make_move(self, position: int) -> bool:
         if self.board[position] == ' ':
             self.board[position] = self.current_player
             return True
         return False
-
     def switch_player(self):
         self.current_player = 'O' if self.current_player == 'X' else 'X'
-
     def check_winner(self) -> str:
         winning_combinations = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -84,7 +60,6 @@ def play(self):
             if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != ' ':
                 return self.board[combo[0]]  # Return the winner
         return None  # No winner yet
-
     def reset_game(self):
         self.board = [' ' for _ in range(9)]
         self.current_player = 'X'
