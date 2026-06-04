7b355085975f025496c9ab6cf61fa85d42264a9c
Satwik1Anand <satwik.anand@infogain.com>
2026-06-04 14:05:57 +0530

    [AT-7] Add utility function: string_utils.reverse_string()

--- add_utility_function_stringutilsreversestring.py
+++ add_utility_function_stringutilsreversestring.py
@@ -1,6 +1,15 @@
 # string_utils.py
 
 def reverse_string(text: str) -> str:
+    """Return the reversed string of the given text."""
+    if text == '':
+        return ''
+    return text[::-1]
+# HOW TO RUN:
+# pip install -r requirements.txt
+# python add_utility_function_stringutilsreversestring.py
+# Then open: http://127.0.0.1:5000
+
     """Reverses the given string.
 
     Args:
